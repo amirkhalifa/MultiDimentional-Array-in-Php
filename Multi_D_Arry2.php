5f1fcfcae77fdd0ba49333719d3ddcae8673b556
amirkhalifa <hossainamir590@gmail.com>
2019-05-30 00:03:01 +0600

    first commit

---
+++ Multi_D_Arry2.php
@@ -0,0 +1,14 @@
+<?php
+
+$students =array(
+	array("Mark",2011,'33th','abc@gmail.com'),
+	array("Rabi",2001,'38th','def@yahoo.com'),
+	array("Pavel",2011,'40th','hij@zoho.com'),
+	array("Imran",2021,'41th','xyz@gmail.com')
+);
+
+echo $students[0][0].'	';	echo $students[0][1].'||';echo $students[0][2].'  ';echo $students[0][3].'<br><br>';
+echo $students[1][0].'	';	echo $students[1][1].'||';echo $students[1][2].'  ';echo $students[1][3].'<br><br>';
+echo $students[2][0].'	';	echo $students[2][1].'||';echo $students[2][2].'  ';echo $students[2][3].'<br><br>';
+echo $students[3][0].'	';	echo $students[3][1].'||';echo $students[3][2].'  ';echo $students[3][3].'<br><br>';
+?>
